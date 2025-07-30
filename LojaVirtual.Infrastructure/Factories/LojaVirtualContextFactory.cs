9430448251d3dfd6a5f5c082185f3d5aac7a393f
Gustavo <Gustavo@WIN-216HVE2K6U1>
2025-07-30 15:45:28 -0300

    #001 - Context Integração Azure

---
+++ LojaVirtual.Infrastructure/Factories/LojaVirtualContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace LojaVirtual.Infrastructure.Models
+{
+    public class LojaVirtualContextFactory : IDesignTimeDbContextFactory<LojaVirtualContext>
+    {
+        public LojaVirtualContext CreateDbContext(string[] args)
+        {
+            // Lê o appsettings.json
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../LojaVirtual"))
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<LojaVirtualContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new LojaVirtualContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ LojaVirtual.Infrastructure/Models/LojaVirtualContext.cs
@@ -0,0 +1,23 @@
+﻿using LojaVirtual.Domain.Entities; // Ajuste para o namespace correto das entidades
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaVirtual.Infrastructure.Models
+{
+    public partial class LojaVirtualContext : DbContext
+    {
+        public LojaVirtualContext(DbContextOptions<LojaVirtualContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LojaVirtualContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
