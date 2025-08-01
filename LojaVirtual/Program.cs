5fcf3865e94088c340d491c0ee702af10c23b012
Gustavo <Gustavo@WIN-216HVE2K6U1>
2025-07-31 23:06:58 -0300

    Reformulação projeto API

--- LojaVirtual/Program.cs
+++ LojaVirtual/Program.cs
@@ -1,34 +1,31 @@
+using LojaVirtual.Domain.Interfaces;
+using LojaVirtual.Infrastructure.Models;
+using LojaVirtual.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 
-namespace LojaVirtual
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// DbContext Azure SQL
+builder.Services.AddDbContext<LojaVirtualContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureConnection")));
+
+// Repository
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
